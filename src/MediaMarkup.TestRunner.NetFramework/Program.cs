08589b594636359d451f42fb25e0f2d1294a6c90
Mahmut Canga <cangamahmut@gmail.com>
2019-09-21 22:14:16 +0100

    created test runner console app

---
+++ src/MediaMarkup.TestRunner.NetFramework/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaMarkup.TestRunner.NetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Client ID:");
+            string clientId = Console.ReadLine();
+
+            Console.Write("Enter your Client Secret:");
+            string clientScret = Console.ReadLine();
+
+            var settings = new Settings
+            {
+                ApiBaseUrl = "https://localhost:44318",
+                ClientId = clientId,
+                SecretKey = clientScret
+            };
+
+            try
+            {
+                var apiClient = new ApiClient(settings);
+                apiClient.InitializeAsync().Wait();
+
+                SearchUserByEmail(apiClient);
+            }
+            catch (ApiException apie )
+            {
+
+                Console.WriteLine("An error occured...");
+                Console.WriteLine(apie.Message);
+            }
+        }
+
+        private static void SearchUserByEmail(ApiClient apiClient)
+        {
+            Console.Write("Enter an email address to search:");
+            string email = Console.ReadLine();
+
+            Console.WriteLine($"Searching {email}...");
+            var user = apiClient.Users.GetByEmail(email).Result;
+
+            Console.WriteLine($"{user.FirstName} {user.LastName} found.");
+        }
+    }
+}
