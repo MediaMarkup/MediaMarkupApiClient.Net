2a4d34cf6122001abb93036cd9a2a9624fe771a1
Mahmut Canga <cangamahmut@gmail.com>
2019-09-27 00:17:58 +0100

    updated test runner with an easier flow

--- src/MediaMarkup.TestRunner.NetFramework/Program.cs
+++ src/MediaMarkup.TestRunner.NetFramework/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -30,6 +31,8 @@ namespace MediaMarkup.TestRunner.NetFramework
                 SecretKey = clientScret
             };
 
+            
+
             try
             {
                 Console.WriteLine("Authorizing client details and retrieving AccessToken...");
@@ -38,22 +41,68 @@ namespace MediaMarkup.TestRunner.NetFramework
                 Console.WriteLine("AccessToken acquired, ready to drive!");
                 Console.WriteLine($"\n{token}\n");
 
+                Console.WriteLine("Successfully authorized the connection, press any key to continue");
+                Console.ReadKey();
+
                 UserOperations = new UserOperations(apiClient);
                 ApprovalOperations = new ApprovalOperations(apiClient);
 
-                Console.WriteLine("To skip any of the steps, just enter -1 as the input");
+                var programOperations = new Dictionary<int, Func<Task>>
+                {
+                    { 1, UserOperations.InviteUser },
+                    { 2, UserOperations.GetUserByEmail },
+                    { 3, UserOperations.GetUserById },
+                    { 4, UserOperations.UpdateUser },
+                    { 5, UserOperations.DeleteUserById },
 
-                await UserOperations.InviteUser();
-                await UserOperations.GetUserByEmail();
+                    { 6, ApprovalOperations.CreateApproval },
+                    { 7, ApprovalOperations.GetApproval },
+                    { 8, ApprovalOperations.UpdateApproval },
+                    { 9, ApprovalOperations.DeleteApproval },
+                    { 10, ApprovalOperations.ExportApprovalReport }
+                };
 
-                await UserOperations.GetUserById();
-                await UserOperations.UpdateUser();
-                await UserOperations.DeleteUserById();
+                
 
-                await ApprovalOperations.CreateApproval();
-                await ApprovalOperations.GetApproval();
-                await ApprovalOperations.UpdateApproval();
-                await ApprovalOperations.DeleteApproval();
+                int operationSelected = -1;
+
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nChoose an action to run on Public API. (-1 to exit)\n");
+                    foreach (var programOperation in programOperations)
+                    {
+                        Console.WriteLine($"{programOperation.Key}. {programOperation.Value.Method.Name}");
+                    }
+
+                    Console.Write("Run: ");
+                    var validSelection = int.TryParse(Console.ReadLine(), out operationSelected);
+
+                    if (!validSelection)
+                    {
+                        Console.WriteLine("Couldn't recognize the input. Please enter a number to choose from the menu");
+                        continue;
+                    }
+
+                    if (operationSelected == -1) break;
+
+                    try
+                    {
+                        var operation = programOperations[operationSelected];
+
+                        Console.Clear();
+                        await operation();
+                        Console.WriteLine("\n.....press any key to continue...");
+                        Console.ReadKey();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("An exception occured, press any key to continue...");
+                        Console.WriteLine(ex.Message);
+                        Console.ReadKey();
+                    }
+                }
+                while (operationSelected != -1);
                 //GetApprovals();
                 //UpdateApproval();
                 //ExportReport();
@@ -77,7 +126,6 @@ namespace MediaMarkup.TestRunner.NetFramework
 
                 Console.WriteLine("\n!!!!!!!!!!!!!!!!!!!");
                 Console.WriteLine("End of drive...Press any key to close the program.");
-                Console.ReadKey();
             }
             catch (ApiException e )
             {
